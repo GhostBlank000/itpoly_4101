4ae7a8426395abf139c96f55f7928d096e7decaa
GhostBlank000 <markpro904@gmail.com>
2024-12-24 13:35:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
       <section> <Navbar /> </section>
       <section> <Header /> </section>
       <section> <Home /> </section>
-      <section>
-        <iframe src="https://www.lannapoly.ac.th/web/#/personal/" title="Lanna Poly" className="h-screen w-screen" style={{ border: 'none' }}></iframe>
-      </section>
       <section> <Footer /> </section>
     </div>
   );
